143ad1559e384af34088cb7c18db2862a90ce9ee
Gabriel <marquez122@outlook.com>
2021-03-07 14:20:46 -0500

    Spring boot project setup

---
+++ src/main/java/com/ordenada/chikens/ChikensApplication.java
@@ -0,0 +1,13 @@
+package com.ordenada.chikens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChikensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChikensApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ordenada/chikens/ChikensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ordenada.chikens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChikensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
