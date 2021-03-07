7c8f8888d9c44f61a1e39c0bce847ae08fa92d4d
Gabriel <marquez122@outlook.com>
2021-03-07 15:35:18 -0500

    Chicken and Clutch class added

--- src/main/java/com/ordenada/chickens/chicken/Chicken.java
+++ src/main/java/com/ordenada/chickens/chicken/Chicken.java
@@ -1,5 +1,101 @@
 package com.ordenada.chickens.chicken;
 
-public class Chicken {
+import com.ordenada.chickens.clutch.Clutch;
 
+public class Chicken {
+    private Long id;
+    private String name;
+    private Clutch clutch;
+    private String color;
+    private String breed;
+    private String age;
+
+    public Chicken() {
+    }
+
+    public Chicken(Long id,
+                   String name,
+                   Clutch clutch,
+                   String color,
+                   String breed,
+                   String age) {
+        this.id = id;
+        this.name = name;
+        this.clutch = clutch;
+        this.color = color;
+        this.breed = breed;
+        this.age = age;
+    }
+
+    public Chicken(String name,
+                   Clutch clutch,
+                   String color,
+                   String breed,
+                   String age) {
+        this.name = name;
+        this.clutch = clutch;
+        this.color = color;
+        this.breed = breed;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Clutch getClutch() {
+        return clutch;
+    }
+
+    public void setClutch(Clutch clutch) {
+        this.clutch = clutch;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Chicken{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", clutch=" + clutch +
+                ", color='" + color + '\'' +
+                ", breed='" + breed + '\'' +
+                ", age='" + age + '\'' +
+                '}';
+    }
 }
---
+++ src/main/java/com/ordenada/chickens/clutch/Clutch.java
@@ -0,0 +1,90 @@
+package com.ordenada.chickens.clutch;
+
+import com.ordenada.chickens.chicken.Chicken;
+
+import java.util.Date;
+
+public class Clutch {
+    private Long id;
+    private Date startDate;
+    private Date endDate;
+    private Integer eggs;
+    private String eggType;
+    private Chicken chicken;
+
+    public Clutch() {
+    }
+
+    public Clutch(Long id, Date startDate, Date endDate, Integer eggs, String eggType, Chicken chicken) {
+        this.id = id;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.eggs = eggs;
+        this.eggType = eggType;
+        this.chicken = chicken;
+    }
+
+    public Clutch(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public Integer getEggs() {
+        return eggs;
+    }
+
+    public void setEggs(Integer eggs) {
+        this.eggs = eggs;
+    }
+
+    public String getEggType() {
+        return eggType;
+    }
+
+    public void setEggType(String eggType) {
+        this.eggType = eggType;
+    }
+
+    public Chicken getChicken() {
+        return chicken;
+    }
+
+    public void setChicken(Chicken chicken) {
+        this.chicken = chicken;
+    }
+
+    @Override
+    public String toString() {
+        return "Clutch{" +
+                "id=" + id +
+                ", startDate=" + startDate +
+                ", endDate=" + endDate +
+                ", eggs=" + eggs +
+                ", eggType='" + eggType + '\'' +
+                ", chicken=" + chicken +
+                '}';
+    }
+}
